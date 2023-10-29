f028801d97790ecd2dc7a4e44ecf68ed4b8e688a
ftgeorge <ftgeorgean@gmail.com>
2023-10-29 01:09:31 +0100

    feat: Updated styles

--- src/modules/MarketplacePage.tsx
+++ src/modules/MarketplacePage.tsx
@@ -1,4 +1,5 @@
 import useLoadingDelay from '@/hooks/useLoadingDelay';
+import { MarketplaceCards } from '@/ui/cards/marketplacecards';
 import { Loader } from '@/ui/loaders/Loader';
 import { BottomNav } from '@/ui/navigation/BottomNav';
 import { Nav } from '@/ui/navigation/Nav';
@@ -15,6 +16,11 @@ export const MarketplacePage: React.FC<MarketplacePageProps> = () => {
       ) : (
         <>
           <Nav />
+          <div className='w-full flex flex-wrap justify-center gap-6 mt-20 md:mt-40 lg:mt-40 xl:mt-40 2xl:mt-40'>
+          <MarketplaceCards/>
+          <MarketplaceCards/>
+          <MarketplaceCards/>
+          </div>
           <BottomNav />
         </>
       )}
